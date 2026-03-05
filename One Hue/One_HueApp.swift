b55857b04745a12ba44d1549f51792e6bf246a99
Tyler D <tyler.dix@gmail.com>
2026-03-04 19:12:33 -0700

    Initial Commit

---
+++ One Hue/One_HueApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_HueApp.swift
+//  One Hue
+//
+//  Created by Tyler on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct One_HueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
